81d28211537c7a677a59e188406facc55bd9a45d
RuYunW <42913632+RuYunW@users.noreply.github.com>
2019-03-03 14:55:53 +0800

    1

---
+++ 《Java语言程序设计》课后题/2.13.java
@@ -0,0 +1,17 @@
+package sdnu.wry.demo;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("请输入每月存入的金额：");
+		double money = input.nextDouble();//number  of money saving per month
+		System.out.println("请输入存款月数：");
+		int m=input.nextInt();//number month of  money saving
+		double sumMoney=0;
+		for ( int i = 0 ; i < m ; i++ ){
+			sumMoney=(money+sumMoney)*(1+0.00417);
+		}
+		System.out.println(m+"个月后，账户上的钱数为"+sumMoney);
+
+    }
+}
